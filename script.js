0513d538689c07c6858f9b1d477c8abc3f5c83f6
Kareem Alsayd <106820544+kemooalsayd@users.noreply.github.com>
2022-10-18 09:08:58 +0200

    add some sections
    add pricing section and adeit footer

---
+++ script.js
@@ -0,0 +1,22 @@
+let copyr=document.getElementById('copyright')
+const d = new Date();
+let year = d.getFullYear();
+
+copyr.innerHTML=`
+© ${year} Copyright saved: 
+<!-- Github -->
+<a
+  class="btn text-white btn-floating fs-3 m-1"
+  style="background-color: #333333;"
+  href="https://github.com/kemooalsayd" 
+  target="_blank"
+  role="button"
+  ><i class="fab fa-github"></i
+></a>
+`
+
+
+
+setTimeout(() => {
+    alert('يا عمر يا وحش يالي مش مصدقني')
+}, 1000);
